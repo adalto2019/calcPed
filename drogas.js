82c0f0cfe87aa823f45575e3fa92cfcebf67ff51
Adalto Jr <adaltojr.aj@gmail.com>
2024-03-30 23:02:10 -0300

    Atualização: Drogas para sedação / Analgesia

---
+++ drogas.js
@@ -0,0 +1,192 @@
+function functionFentanil() {
+    document.getElementById("myDropdownFentanil").classList.toggle("show");
+}
+function functionLidocaina() {
+  document.getElementById("myDropdownLidocaina").classList.toggle("show");
+}
+function functionMorfina() {
+  document.getElementById("myDropdownMorfina").classList.toggle("show");
+}
+
+    // Close the dropdown menu if the user clicks outside of it
+    window.onclick = function(event) {
+      if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+          var openDropdown = dropdowns[i];
+          if (openDropdown.classList.contains('show')) {
+            openDropdown.classList.remove('show');
+          }
+        }
+      }
+    }
+  
+// Fentanil ###################################################################################################
+function fentanil1(){
+alert("FENTANIL - Frasco Ampola: 2.5 e 10ml com 50mcg/ml\n"+
+"\nPrescrição: Administrar 0.12ml diluída em 3ml de soro fisiológico (bolus EV lento de 3 minutos) Essa dose corresponde a 2 mcg/kg.\n"+
+"\nCálculo: Peso x 0.04.")
+}
+function fentanil2(){
+  alert("FENTANIL - Frasco Ampola: 2.5 e 10ml com 50mcg/ml\n"+
+  "\nPrescrição: Administrar 0.16ml diluída em 3ml de soro fisiológico (bolus EV lento de 3 minutos) Essa dose corresponde a 2 mcg/kg.\n"+
+  "\nCálculo: Peso x 0.04.")
+}
+function fentanil3(){
+alert("FENTANIL - Frasco Ampola: 2.5 e 10ml com 50mcg/ml\n"+
+"\nPrescrição: Administrar 0.2ml diluída em 3ml de soro fisiológico (bolus EV lento de 3 minutos) Essa dose corresponde a 2 mcg/kg.\n"+
+"\nCálculo: Peso x 0.04.")
+}
+function fentanil4(){
+alert("FENTANIL - Frasco Ampola: 2.5 e 10ml com 50mcg/ml\n"+
+"\nPrescrição: Administrar 0.25ml diluída em 3ml de soro fisiológico (bolus EV lento de 3 minutos) Essa dose corresponde a 2 mcg/kg.\n"+
+"\nCálculo: Peso x 0.04.")
+}
+function fentanil5(){
+alert("FENTANIL - Frasco Ampola: 2 - 5 e 10ml com 50mcg/ml\n"+
+"\nPrescrição: Administrar 0.3ml diluída em 3ml de soro fisiológico (bolus EV lento de 3 minutos) Essa dose corresponde a 2 mcg/kg.\n"+
+"\nCálculo: Peso x 0.04.")
+}
+function fentanil6(){
+alert("FENTANIL - Frasco Ampola: 2 - 5 e 10ml com 50mcg/ml\n"+
+"\nPrescrição: Administrar 0.4ml diluída em 3ml de soro fisiológico (bolus EV lento de 3 minutos) Essa dose corresponde a 2 mcg/kg.\n"+
+"\nCálculo: Peso x 0.04.")
+}
+function fentanil7(){
+alert("FENTANIL - Frasco Ampola: 2 - 5 e 10ml com 50mcg/ml\n"+
+"\nPrescrição: Administrar 0.5ml diluída em 3ml de soro fisiológico (bolus EV lento de 3 minutos) Essa dose corresponde a 2 mcg/kg.\n"+
+"\nCálculo: Peso x 0.04.")
+}
+function fentanil8(){
+alert("FENTANIL - Frasco Ampola: 2 - 5 e 10ml com 50mcg/ml\n"+
+"\nPrescrição: Administrar 0.6ml diluída em 3ml de soro fisiológico (bolus EV lento de 3 minutos) Essa dose corresponde a 2 mcg/kg.\n"+
+"\nCálculo: Peso x 0.04.")
+}
+function fentanil9(){
+alert("FENTANIL - Frasco Ampola: 2 - 5 e 10ml com 50mcg/ml\n"+
+"\nPrescrição: Administrar 0.8ml diluída em 3ml de soro fisiológico (bolus EV lento de 3 minutos) Essa dose corresponde a 2 mcg/kg.\n"+
+"\nCálculo: Peso x 0.04.")
+}
+function fentanil10(){
+alert("FENTANIL - Frasco Ampola: 2 - 5 e 10ml com 50mcg/ml\n"+
+"\nPrescrição: Administrar 1.0ml diluída em 3ml de soro fisiológico (bolus EV lento de 3 minutos) Essa dose corresponde a 2 mcg/kg.\n"+
+"\nCálculo: Peso x 0.04.")
+}
+function fentanil11(){
+alert("FENTANIL - Frasco Ampola: 2 - 5 e 10ml com 50mcg/ml\n"+
+"\nPrescrição: Administrar 1.4ml diluída em 3ml de soro fisiológico (bolus EV lento de 3 minutos) Essa dose corresponde a 2 mcg/kg.\n"+
+"\nCálculo: Peso x 0.04.")
+}
+// Lidocaína ###################################################################################################
+function lidocaina1(){
+alert("LIDOCAÍNA - a 1% sem vasoconstritor. Frascos de 2 a 20ml com 10mg/ml\n"+
+"\nPrescrição: Administrar EV lento 0.3ml diluída em 3-5ml de soro fisiológico (Essa dose = 1mg/kg). Para intubação: 0.3 a 0.6ml.\n"+
+"\nCálculo: Peso x 0.1.")
+}
+function lidocaina2(){
+alert("LIDOCAÍNA - a 1% sem vasoconstritor. Frascos de 2 a 20ml com 10mg/ml\n"+
+"\nPrescrição: Administrar EV lento 0.4ml diluída em 3-5ml de soro fisiológico (Essa dose = 1mg/kg). Para intubação: 0.4 a 0.8ml.\n"+
+"\nCálculo: Peso x 0.1.")
+}
+function lidocaina3(){
+alert("LIDOCAÍNA - a 1% sem vasoconstritor. Frascos de 2 a 20ml com 10mg/ml\n"+
+"\nPrescrição: Administrar EV lento 0.5ml diluída em 3-5ml de soro fisiológico (Essa dose = 1mg/kg). Para intubação: 0.5 a 1.0ml.\n"+
+"\nCálculo: Peso x 0.1.")
+}
+function lidocaina4(){
+alert("LIDOCAÍNA - a 1% sem vasoconstritor. Frascos de 2 a 20ml com 10mg/ml\n"+
+"\nPrescrição: Administrar EV lento 0.6ml diluída em 3-5ml de soro fisiológico (Essa dose = 1mg/kg). Para intubação: 0.6 a 1.2ml.\n"+
+"\nCálculo: Peso x 0.1.")
+}
+function lidocaina5(){
+alert("LIDOCAÍNA - a 1% sem vasoconstritor. Frascos de 2 a 20ml com 10mg/ml\n"+
+"\nPrescrição: Administrar EV lento 0.8ml diluída em 3-5ml de soro fisiológico (Essa dose = 1mg/kg). Para intubação: 0.8 a 1.6ml.\n"+
+"\nCálculo: Peso x 0.1.")
+}
+function lidocaina6(){
+alert("LIDOCAÍNA - a 1% sem vasoconstritor. Frascos de 2 a 20ml com 10mg/ml\n"+
+"\nPrescrição: Administrar EV lento 1.0ml diluída em 3-5ml de soro fisiológico (Essa dose = 1mg/kg). Para intubação: 1.0 a 2.0ml.\n"+
+"\nCálculo: Peso x 0.1.")
+}
+function lidocaina7(){
+alert("LIDOCAÍNA - a 1% sem vasoconstritor. Frascos de 2 a 20ml com 10mg/ml\n"+
+"\nPrescrição: Administrar EV lento 1.2ml diluída em 3-5ml de soro fisiológico (Essa dose = 1mg/kg). Para intubação: 1.2 a 2.4ml.\n"+
+"\nCálculo: Peso x 0.1.")
+}
+function lidocaina8(){
+alert("LIDOCAÍNA - a 1% sem vasoconstritor. Frascos de 2 a 20ml com 10mg/ml\n"+
+"\nPrescrição: Administrar EV lento 1.6ml diluída em 3-5ml de soro fisiológico (Essa dose = 1mg/kg). Para intubação: 1.6 a 3.2ml.\n"+
+"\nCálculo: Peso x 0.1.")
+}
+function lidocaina9(){
+alert("LIDOCAÍNA - a 1% sem vasoconstritor. Frascos de 2 a 20ml com 10mg/ml\n"+
+"\nPrescrição: Administrar EV lento 2.0ml diluída em 3-5ml de soro fisiológico (Essa dose = 1mg/kg). Para intubação: 2.0 a 4.0ml.\n"+
+"\nCálculo: Peso x 0.1.")
+}
+function lidocaina10(){
+alert("LIDOCAÍNA - a 1% sem vasoconstritor. Frascos de 2 a 20ml com 10mg/ml\n"+
+"\nPrescrição: Administrar EV lento 2.5ml diluída em 3-5ml de soro fisiológico (Essa dose = 1mg/kg). Para intubação: 2.5 a 5.0ml.\n"+
+"\nCálculo: Peso x 0.1.")
+}
+function lidocaina11(){
+alert("LIDOCAÍNA - a 1% sem vasoconstritor. Frascos de 2 a 20ml com 10mg/ml\n"+
+"\nPrescrição: Administrar EV lento 3.5ml diluída em 3-5ml de soro fisiológico (Essa dose = 1mg/kg). Para intubação: 3.5 a 7.0ml.\n"+
+"\nCálculo: Peso x 0.1.")
+}
+// Morfina ###################################################################################################
+function morfina1(){
+  alert("MORFINA - Usar ampola de 1mg/ml (e não 10mg/ml)\n"+
+  "\nPrescrição: Administrar EV lento 0.3ml (diluir em 3ml de ABD  se preciso).(Essa dose é de 0.1mg/kg é a dose básica para analgesia).\n"+
+  "\nCálculo: Peso x 0.1.")
+}
+function morfina2(){
+  alert("MORFINA - Usar ampola de 1mg/ml (e não 10mg/ml)\n"+
+  "\nPrescrição: Administrar EV lento 0.4ml (diluir em 3ml de ABD  se preciso).(Essa dose é de 0.1mg/kg é a dose básica para analgesia).\n"+
+  "\nCálculo: Peso x 0.1.")
+}
+function morfina3(){
+  alert("MORFINA - Usar ampola de 1mg/ml (e não 10mg/ml)\n"+
+  "\nPrescrição: Administrar EV lento 0.5ml (diluir em 3ml de ABD  se preciso).(Essa dose é de 0.1mg/kg é a dose básica para analgesia).\n"+
+  "\nCálculo: Peso x 0.1.")
+}
+function morfina4(){
+  alert("MORFINA - Usar ampola de 1mg/ml (e não 10mg/ml)\n"+
+  "\nPrescrição: Administrar EV lento 0.6ml (diluir em 3ml de ABD  se preciso).(Essa dose é de 0.1mg/kg é a dose básica para analgesia).\n"+
+  "\nCálculo: Peso x 0.1.")
+}
+function morfina5(){
+  alert("MORFINA - Usar ampola de 1mg/ml (e não 10mg/ml)\n"+
+  "\nPrescrição: Administrar EV lento 0.8ml (diluir em 3ml de ABD  se preciso).(Essa dose é de 0.1mg/kg é a dose básica para analgesia).\n"+
+  "\nCálculo: Peso x 0.1.")
+}
+function morfina6(){
+  alert("MORFINA - Usar ampola de 1mg/ml (e não 10mg/ml)\n"+
+  "\nPrescrição: Administrar EV lento 1.0ml (diluir em 3ml de ABD  se preciso).(Essa dose é de 0.1mg/kg é a dose básica para analgesia).\n"+
+  "\nCálculo: Peso x 0.1.")
+}
+function morfina7(){
+  alert("MORFINA - Usar ampola de 1mg/ml (e não 10mg/ml)\n"+
+  "\nPrescrição: Administrar EV lento 1.2ml (diluir em 3ml de ABD  se preciso).(Essa dose é de 0.1mg/kg é a dose básica para analgesia).\n"+
+  "\nCálculo: Peso x 0.1.")
+}
+function morfina8(){
+  alert("MORFINA - Usar ampola de 1mg/ml (e não 10mg/ml)\n"+
+  "\nPrescrição: Administrar EV lento 1.6ml (diluir em 3ml de ABD  se preciso).(Essa dose é de 0.1mg/kg é a dose básica para analgesia).\n"+
+  "\nCálculo: Peso x 0.1.")
+}
+function morfina9(){
+  alert("MORFINA - Usar ampola de 1mg/ml (e não 10mg/ml)\n"+
+  "\nPrescrição: Administrar EV lento 2.0ml (diluir em 3ml de ABD  se preciso).(Essa dose é de 0.1mg/kg é a dose básica para analgesia).\n"+
+  "\nCálculo: Peso x 0.1.")
+}
+function morfina10(){
+  alert("MORFINA - Usar ampola de 1mg/ml (e não 10mg/ml)\n"+
+  "\nPrescrição: Administrar EV lento 2.5ml (diluir em 3ml de ABD  se preciso).(Essa dose é de 0.1mg/kg é a dose básica para analgesia).\n"+
+  "\nCálculo: Peso x 0.1.")
+}
+function morfina11(){
+  alert("MORFINA - Usar ampola de 1mg/ml (e não 10mg/ml)\n"+
+  "\nPrescrição: Administrar EV lento 3.5ml (diluir em 3ml de ABD  se preciso).(Essa dose é de 0.1mg/kg é a dose básica para analgesia).\n"+
+  "\nCálculo: Peso x 0.1.")
+}
